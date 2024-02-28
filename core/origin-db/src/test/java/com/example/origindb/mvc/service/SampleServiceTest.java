f5903704378d2c6a8cf4355497be467fba8e8f56
twocowsong <twocowsong@naver.com>
2024-02-28 21:23:09 +0900

    feat: [admin, front] 테스트코드 추가 및 작업 반영

---
+++ core/origin-db/src/test/java/com/example/origindb/mvc/service/SampleServiceTest.java
@@ -0,0 +1,9 @@
+package com.example.origindb.mvc.service;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+
+@WebMvcTest(SampleService.class)
+class SampleServiceTest {
+
+}
